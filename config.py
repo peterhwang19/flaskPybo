75bf6ab2c5e3a674c6830054a814f29825d91dd6
Peter Hwang <peterhwang@Peters-MacBook-Pro-2.local>
2021-01-26 21:20:52 +0900

    first

---
+++ config.py
@@ -0,0 +1,7 @@
+import os
+
+BASE_DIR = os.path.dirname(__file__)
+# BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+
+SQLALCHEMY_DATABASE_URI = 'sqlite:///{}'.format(os.path.join(BASE_DIR, 'pybo.db'))
+SQLALCHEMY_TRACK_MODIFICATIONS = False
